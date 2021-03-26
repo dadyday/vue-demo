efd9c8dd2e38e23af02fad3f0dd74bc5f625d004
s0574186 <elisabeth.kintzel@student.htw-berlin.de>
2021-03-26 08:42:55 +0100

    browser-sync

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,25 @@
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+const plugins = [];
+if (process.env.NODE_ENV === "development") {
+  plugins.push(
+    new BrowserSyncPlugin(
+      {
+        host: "localhost",
+        port: 3000,
+        //server: { baseDir: ['public'] }
+        proxy: "http://localhost:8080/",
+      },
+      {
+        reload: false,
+        injectCss: true,
+      }
+    )
+  );
+}
+
 module.exports = {
+  configureWebpack: {
+    plugins: plugins,
+  },
 };
