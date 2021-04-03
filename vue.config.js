3343a0dd22edb7bf673e39b843e6c3685d9a3b36
s0574186 <elisabeth.kintzel@student.htw-berlin.de>
2021-04-03 08:25:34 +0200

    change output dir

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,44 @@
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-
-const plugins = [];
-if (process.env.NODE_ENV === "development") {
-  plugins.push(
-    new BrowserSyncPlugin(
-      {
-        host: "localhost",
-        port: 3000,
-        //server: { baseDir: ['public'] }
-        proxy: "http://localhost:8080/",
-      },
-      {
-        reload: false,
-        injectCss: true,
-      }
-    )
-  );
-}
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  configureWebpack: {
-    plugins: plugins,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'src/public/index.html',
+    },
+  },
+  outputDir: 'public',
+  devServer: {
+    writeToDisk: file => {
+      return !/\.hot-update\.js/.test(file);
+    },
+    watchOptions: {
+      ignored: [
+        'node_modules',
+        'public'
+      ]
+    },
+  },
+  chainWebpack: config => {
+    config.plugins.delete('copy');
+    config.plugin('copy').use(CopyPlugin, [[{
+      from: __dirname+"/src/public",
+      to: __dirname+"/public"
+    }]]);
+    if (process.env.NODE_ENV === "development") {
+      config.plugin('browsersync').use(BrowserSyncPlugin, [
+        {
+          host: "localhost",
+          port: 3000,
+          //server: { baseDir: ['public'] }
+          proxy: "http://localhost:8080/",
+        },
+        {
+          reload: false,
+          injectCss: true,
+        },
+      ]);
+    }
   },
 };
