b53f66729b2844806ef5d9da3279089b59b602d1
s0574186 <elisabeth.kintzel@student.htw-berlin.de>
2021-04-04 15:09:53 +0200

    use fa icons

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <RenderDemo />
+    <FontAwesomeIcon icon="user-secret" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fab);
+// https://fontawesome.com/icons?d=gallery&p=2&s=brands,solid
+// https://github.com/FortAwesome/vue-fontawesome#usage
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+Vue.component("FontAwesomeIcon", FontAwesomeIcon);
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
